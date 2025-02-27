00439e824a09e00e3e588bcbd523104975630cb9
Yura Bobrov <yuryi.babrou@fabros-team.com>
2025-02-27 10:32:52 +0300

    feat: fix

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -16,7 +16,7 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
+      {/* NOTE: Uncomment this code in Chapter 9  */}
 
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
